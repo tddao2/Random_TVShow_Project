b4353424ef951bd52e97f112df0cb672945bd9a8
Cristian Dao <cdao@cougarnet.uh.edu>
2022-08-22 14:55:21 -0500

    Setting EJS view engine

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 const port = 3000;
 app.listen(port, () => {
